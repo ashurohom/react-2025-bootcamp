191c227a086dbe09bd49df3601ed2fff9df87af6
ashurohom <ashitosh.rohom@gmail.com>
2025-07-15 21:23:01 +0530

    add state

--- project-one/src/welcome.jsx
+++ project-one/src/welcome.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+
 function Welcome(props) {
+    const [count, setCount] = useState(0)
+
+    function handelClick(){
+        setCount(count+1);
+    }
+
+    function handelClicks(){
+        setCount(count-1);
+    }
+
+    function handelClickss(){
+        setCount(0);
+    }
+
     return (
      <>
      <h3>WelCome {props.name}</h3>
      <h1>{props.course}</h1>
+
+      <h1>Button Click Count Is {count}</h1>  
+     <button onClick={handelClick}>CLick Me + </button>
+     <button onClick={handelClicks}>CLick Me - </button>
+     <button onClick={handelClickss}>CLick Me Reset </button>
      </>   
     )
 }
