29773bd816ce0764684d69a46c2b8f92cf0971ba
ashurohom <ashitosh.rohom@gmail.com>
2025-07-15 20:54:29 +0530

    project one

---
+++ project-one/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Welcome from './welcome'
+
+function App() {
+  return (
+    <>
+      <h1>Ashitosh Rohom</h1>
+      <Welcome name="Ashu" course="React"/>
+    </>
+  )
+}
+
+export default App
---
+++ project-one/src/welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome(props) {
+    return (
+     <>
+     <h3>WelCome {props.name}</h3>
+     <h1>{props.course}</h1>
+     </>   
+    )
+}
+export default Welcome
