212ee224723bcdb3aa84bf52f76954d4cc3d7e45
ashurohom <ashitosh.rohom@gmail.com>
2025-07-16 20:12:02 +0530

    conditional rendering

--- react-2025/src/components/conditional.jsx
+++ react-2025/src/components/conditional.jsx
@@ -1,3 +1,11 @@
 function Conditional(){
+    const isLoggedIn = true
+
+        if(isLoggedIn){
+            return <h1>WelCome User</h1>
+        }else{
+            return <h1>Please Log In</h1>
+        }
     
 }
+export default Conditional
--- react-2025/src/main.jsx
+++ react-2025/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App.jsx'
 import Footer from './components/Footer.jsx'
+import Conditional from './components/conditional.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Footer />
+    <Conditional />
   </StrictMode>,
 )
