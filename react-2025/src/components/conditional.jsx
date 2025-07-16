96d10596fcb018670eff873c54c13f66ae1f474e
ashurohom <ashitosh.rohom@gmail.com>
2025-07-16 20:30:25 +0530

    use conditional rendering

--- react-2025/src/components/conditional.jsx
+++ react-2025/src/components/conditional.jsx
@@ -1,11 +1,17 @@
 function Conditional(){
     const isLoggedIn = true
 
-        if(isLoggedIn){
-            return <h1>WelCome User</h1>
-        }else{
-            return <h1>Please Log In</h1>
-        }
+        // if(isLoggedIn){
+        //     return <h1>WelCome User</h1>
+        // }else{
+        //     return <h1>Please Log In</h1>
+        // }
     
+        return(
+        <div>
+            <h1>Hello, React!</h1>
+            {isLoggedIn && <p>User is loggedin</p>}
+        </div>
+        );
 }
 export default Conditional
