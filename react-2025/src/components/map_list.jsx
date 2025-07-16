9dfc3095f8688344cb9e9d40d8326b07145791e1
ashurohom <ashitosh.rohom@gmail.com>
2025-07-16 21:49:17 +0530

    map list

--- react-2025/src/components/map_list.jsx
+++ react-2025/src/components/map_list.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
+// function Map_list() {
+//     const names = ["Ashu","Soham","Vraj",]
+//   return (
+//     // <div>
+//     //   <h3>{names[0]}</h3>
+//     //   <h3>{names[1]}</h3>
+//     //   <h3>{names[2]}</h3>
+//     // </div>
+//   )
+// }
+// export default Map_list
+
 function Map_list() {
     const names = ["Ashu","Soham","Vraj",]
-  return (
-    <div>
-      <h3>{names[0]}</h3>
-      <h3>{names[1]}</h3>
-      <h3>{names[2]}</h3>
-    </div>
-  )
+    const nameList = names.map(name => <h3>{name}</h3>)
+  return(<div>{nameList}</div>)
 }
 export default Map_list
