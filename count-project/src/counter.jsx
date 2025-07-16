9c512dcd567720fe0113bf56fb4a32d445f31ac9
ashurohom <ashitosh.rohom@gmail.com>
2025-07-16 17:56:51 +0530

    update button styling

--- count-project/src/counter.jsx
+++ count-project/src/counter.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import './app.css'
 
 function Counter(){
     const [count, setCount] = useState(0)
@@ -13,11 +14,11 @@ function Counter(){
     }
     return(
         <>
-            <h6>Counter</h6>
+            <h6 className="name">Counter</h6>
             <h3>Count Is {count}</h3>
-            <button onClick={button}>CLick Me + </button>
-            <button onClick={button1}>CLick Me - </button>
-            <button onClick={button2}>Reset</button>
+            <button className="b1" onClick={button}>CLick Me + </button>
+            <button className="b2" onClick={button1}>CLick Me - </button>
+            <button className="b3" onClick={button2}>Reset</button>
      </>   
     )
 }
