7cc19cb2bc1963a8f2f51d105e6b2b33d00445ca
ashurohom <ashitosh.rohom@gmail.com>
2025-07-17 20:54:45 +0530

    new component

---
+++ HelloWorld/src/components/Hello.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css'
+
+export const Hello = () => {
+    return (
+        <div>
+            <h1>Hello Ashu, WelCome React Tutorials</h1>
+        </div>
+    );
+}
--- HelloWorld/src/main.jsx
+++ HelloWorld/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App, Newapp } from './App.jsx'
+// import { App, Newapp } from './App.jsx'
+import { Hello } from './components/Hello.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Newapp />
+    {/* <App /> */}
+    {/* <Newapp /> */}
+    {<Hello />}
   </StrictMode>,
 )
