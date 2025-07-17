8edd45c505449372785560ce92551308b78d0f85
ashurohom <ashitosh.rohom@gmail.com>
2025-07-17 23:00:02 +0530

    useState Example

--- HelloWorld/src/components/Name.jsx
+++ HelloWorld/src/components/Name.jsx
@@ -5,7 +5,9 @@ function Name() {
     const [name, setName] = useState('')
   return (
     <div>
-      
+      <input type='text' placeholder='Your Name' value={name} onChange={(e)=>setName(e.target.value)}/>
+    
+    <h1>Hello, {name}</h1>
     </div>
   )
 }
