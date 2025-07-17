536bb74d28745736a1605a66e7b479f109d23065
ashurohom <ashitosh.rohom@gmail.com>
2025-07-17 21:03:56 +0530

    props

---
+++ HelloWorld/src/components/Props.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Props(props) {
+  return (
+    <div>
+      <h3>Hello,{props.name}</h3>
+    </div>)
+}
+
+export default Props
--- HelloWorld/src/main.jsx
+++ HelloWorld/src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { App, Newapp } from './App.jsx'
 import { Hello } from './components/Hello.jsx'
+import Props from './components/Props.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <Newapp /> */}
     {<Hello />}
+    {<Props name="Ashu" />}
+    {<Props name="Soham" />}
+    {<Props name="Nikhil" />}
+    
   </StrictMode>,
 )
