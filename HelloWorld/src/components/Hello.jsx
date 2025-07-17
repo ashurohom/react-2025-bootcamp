0f5015f6561a7b2f951d63074377a55389e2d29c
ashurohom <ashitosh.rohom@gmail.com>
2025-07-17 21:13:18 +0530

    props

--- HelloWorld/src/components/Hello.jsx
+++ HelloWorld/src/components/Hello.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import '../App.css'
+import Props from "./Props";
 
 export const Hello = () => {
     return (
         <div>
             <h1>Hello Ashu, WelCome React Tutorials</h1>
+            
+            <Props name="Ashu" rollno='10' />
+            <p>You are Great Person</p>
+            
+            <Props name="Soham" rollno='11' />
+            <button>Button</button>
+            
+            <Props name="Nikhil" rollno='12' />
+
         </div>
     );
 }
--- HelloWorld/src/main.jsx
+++ HelloWorld/src/main.jsx
@@ -1,18 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import { App, Newapp } from './App.jsx'
+import { App, Newapp } from './App.jsx'
 import { Hello } from './components/Hello.jsx'
-import Props from './components/Props.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    {/* <Newapp /> */}
-    {<Hello />}
-    {<Props name="Ashu" rollno='10' />}
-    {<Props name="Soham" rollno='11' />}
-    {<Props name="Nikhil" rollno='12' />}
+    {/* <Newapp />  */}
+    <Hello />
+
     
   </StrictMode>,
 )
