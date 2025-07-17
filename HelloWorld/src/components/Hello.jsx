5ce581b6ea0bd18573db806c7cd5170e3a2aea05
ashurohom <ashitosh.rohom@gmail.com>
2025-07-17 21:43:15 +0530

    useState

--- HelloWorld/src/components/Hello.jsx
+++ HelloWorld/src/components/Hello.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import '../App.css'
 import Props from "./Props";
+import { useState } from "react";
 
 export const Hello = () => {
+
+    const [count, setCount] = useState(0)
+
+    function counter(){
+        setCount(count + 1)
+    }
+    
     return (
         <div>
             <h1>Hello Ashu, WelCome React Tutorials</h1>
@@ -11,7 +19,8 @@ export const Hello = () => {
             <p>You are Great Person</p>
             
             <Props name="Soham" rollno='11' />
-            <button>Button</button>
+            <button onClick={counter}>Button</button>
+            <h1>Count = {count} </h1>
             
             <Props name="Nikhil" rollno='12' />
 
