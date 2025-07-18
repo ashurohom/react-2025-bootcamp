98e66ea6cda2db51c35786cc28638a16c7c30972
ashurohom <ashitosh.rohom@gmail.com>
2025-07-18 20:57:28 +0530

    add form

---
+++ HelloWorld/src/components/form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Form() {
+    const [name, setName] = useState('')
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export de
+import { useState } from 'react'fault Form
