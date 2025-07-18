96e0a4ab6e03b7c4f3055cec9bd0cac39736a6ea
ashurohom <ashitosh.rohom@gmail.com>
2025-07-18 21:59:04 +0530

    start useEffect

--- HelloWorld/src/components/UseEffect.jsx
+++ HelloWorld/src/components/UseEffect.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 function UseEffect() {
+
+    const[count, setCount] = useState(0)
+
+    useEffect(() => {
+        console.log("UseEffect is Running");
+        document.title = `you clicked ${count} times`;
+        
+    },[count]);
+
   return (
     <div>
-      
+      <h3>You Clicked {count} Times</h3>
+      <button onClick={() => setCount(count + 1)}> Click Me</button>
     </div>
   )
 }
