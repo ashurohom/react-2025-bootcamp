5c5a3ddd4e8aed57c3c623bd7d79fe27f5d0bd77
ashurohom <ashitosh.rohom@gmail.com>
2025-07-18 21:28:30 +0530

    add form state

--- HelloWorld/src/components/form.jsx
+++ HelloWorld/src/components/form.jsx
@@ -13,15 +13,23 @@ function Form() {
         setPassword(e.target.value)
     };
 
+    const formSubmit = (e) => {
+        e.preventDefault();
+        alert(`Form Submitted \nName: ${name}\nPassword: ${password}`)
+    };
+
   return (
     <div>
         <h2>React Form Example</h2>
+        <form onSubmit={formSubmit}>
         <input type='text' value={name} onChange={handelChange} placeholder='Enter Input'/>
         <input type='password' value={password} onChange={handelPassword} placeholder='Enter Password'/>
 
         <h3>Input Field Message is : {name}</h3>
         <h3>Password Is : {password}</h3>
-      
+
+        <button type='submit'>Submit</button>
+        </form>
     </div>
   )
 }
