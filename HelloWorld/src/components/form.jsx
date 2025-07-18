e89484ca423af1b44742c9d77974d44ee8aeda7e
ashurohom <ashitosh.rohom@gmail.com>
2025-07-18 21:16:00 +0530

    add form state

--- HelloWorld/src/components/form.jsx
+++ HelloWorld/src/components/form.jsx
@@ -3,16 +3,24 @@ import { useState } from 'react'
 
 function Form() {
     const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
 
     const handelChange = (event) => {
         setName(event.target.value)
+    };
+
+    const handelPassword = (e) => {
+        setPassword(e.target.value)
     };
 
   return (
     <div>
         <h2>React Form Example</h2>
         <input type='text' value={name} onChange={handelChange} placeholder='Enter Input'/>
+        <input type='password' value={password} onChange={handelPassword} placeholder='Enter Password'/>
+
         <h3>Input Field Message is : {name}</h3>
+        <h3>Password Is : {password}</h3>
       
     </div>
   )
