3226e8082ec5139c66460a0f19a8f06eaf07aa7a
ashurohom <ashitosh.rohom@gmail.com>
2025-07-22 12:05:23 +0530

    using async and await fetch data

--- HelloWorld/src/components/Fetch_API.jsx
+++ HelloWorld/src/components/Fetch_API.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Fetch_API() {
     const [data, setData] = useState([])
 
-    
+    useEffect(()=>{
+        const fetchdata = async () =>{
+            const res = await fetch('')
+        }
+    }, []);
+
   return (
     <div>
       
