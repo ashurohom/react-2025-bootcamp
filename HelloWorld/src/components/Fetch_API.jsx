edf878bf80762dc331ff8d4d238c99464991ed3f
ashurohom <ashitosh.rohom@gmail.com>
2025-07-22 12:26:13 +0530

    github data fetch

--- HelloWorld/src/components/Fetch_API.jsx
+++ HelloWorld/src/components/Fetch_API.jsx
@@ -5,13 +5,22 @@ function Fetch_API() {
 
     useEffect(()=>{
         const fetchdata = async () =>{
-            const res = await fetch('')
+            const res = await fetch('https://api.github.com/users')
+            const data = await res.json();
+        setData(data);
         }
+        fetchdata();
     }, []);
 
   return (
     <div>
-      
+      <h2>Github User Data</h2>
+        <ul>
+            {data.map(user => (
+                <li key={user.id}>{user.login}</li>
+        ))}
+        </ul>
+
     </div>
   )
 }
