60a77777698ea3b812108e2dc23ad54bdd508143
ashurohom <ashitosh.rohom@gmail.com>
2025-08-06 10:15:25 +0530

    change props

--- HelloWorld/src/components/Props.jsx
+++ HelloWorld/src/components/Props.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Props(props) {
   return (
     <div>
-      <h3>Hello,{props.name} Your Roll Number is {props.rollno}</h3>
+      <h3>Hello,{props.name} Your Roll Number is {props.rollno}.</h3>
     </div>)
 }
 
