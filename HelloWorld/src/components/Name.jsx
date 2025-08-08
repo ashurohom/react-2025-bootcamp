a7e0bf99cb832ac12e8f308c1ff4c0c67aa4b23a
ashurohom <ashitosh.rohom@gmail.com>
2025-08-08 09:47:59 +0530

    update name

--- HelloWorld/src/components/Name.jsx
+++ HelloWorld/src/components/Name.jsx
@@ -7,7 +7,7 @@ function Name() {
     <div>
       <input type='text' placeholder='Your Name' value={name} onChange={(e)=>setName(e.target.value)}/>
     
-    <h1>Hello, {name}</h1>
+    <h1>Hello, Dear {name}</h1>
     </div>
   )
 }
