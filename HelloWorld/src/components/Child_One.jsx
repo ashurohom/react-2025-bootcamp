7312f029760f707893064660de6af515c3821d2f
ashurohom <ashitosh.rohom@gmail.com>
2025-08-10 22:43:47 +0530

    Change Child One

--- HelloWorld/src/components/Child_One.jsx
+++ HelloWorld/src/components/Child_One.jsx
@@ -1,7 +1,7 @@
 function Child_One({setUser}) {
   return (
     <div>
-      <p>Child One</p>
+      <p>Child One 1</p>
       <input type='text' onChange={(e) => setUser(e.target.value)} placeholder='Enter User'></input>
       <hr />
     </div>
