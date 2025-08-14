ff3deb55c62da976203c3a2a3243b140a378624e
ashurohom <ashitosh.rohom@gmail.com>
2025-08-14 22:01:04 +0530

    add webpage title

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>KrushiVaibhav Farm</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
