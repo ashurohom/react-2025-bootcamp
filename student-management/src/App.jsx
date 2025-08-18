d0214562e3a7cbb0da2afa7e9ecf77865e775213
ashurohom <ashitosh.rohom@gmail.com>
2025-08-18 21:57:12 +0530

    update student-mgnr

--- student-management/src/App.jsx
+++ student-management/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import Students from "./Pages/Student";
+import About from "./Pages/About";
+import "./Navbar.css";   // <-- Import the CSS file
 
 function App() {
   return (
     <div>
-      <h1>Welcome to Student Management App</h1>
+      {/* Navbar */}
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/students">Students</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
