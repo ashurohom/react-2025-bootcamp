58bbd510b7ada769555c46a540bc9c955c1aef63
ashurohom <ashitosh.rohom@gmail.com>
2025-08-22 22:09:45 +0530

    add navbar

--- contact-form-app/src/App.jsx
+++ contact-form-app/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'  
+import './App.css'
+
+function Home() {
+  return <h2>Home</h2>
+} 
+
+function Contact() {
+  return <h2>Contact</h2>
+}
+
 
 function App() {
   return (
     <div>
-      <p>Contact Form App</p>
+      <nav className="navbar">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/contact" className="nav-link">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   )
 }
