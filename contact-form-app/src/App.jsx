b205e6f1e3dff63af865e714fd8cb63590040f74
ashurohom <ashitosh.rohom@gmail.com>
2025-08-22 23:03:55 +0530

    integrate signup page

--- contact-form-app/src/App.jsx
+++ contact-form-app/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Home from './Home'
 import Contact from './Contact'
+import Signup from './Signup'
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
       <nav className="navbar">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/contact" className="nav-link">Contact</Link>
+        <Link to="/signup" className="nav-link">Signup</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   )
